6d912f416b1f168dd3b7f0df20ff23e3f1e1b503
Shweta1702 <98207035+Shweta1702@users.noreply.github.com>
2022-06-17 21:35:52 +0300

    Create playerwithweapon.cs

---
+++ playerwithweapon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    float bulletSpeed = 60;
+    public GameObject bullet;
+
+void Fire(){
+  GameObject tempBullet = Instantiate (bullet, transform.position, transform.rotation) as GameObject;
+  Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+  tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+  Destroy(tempBullet, 5f);
+}
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+          Fire();
+          
+        }
+    }
+}
